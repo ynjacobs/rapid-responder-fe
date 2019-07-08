f52e861e653c8d3db6136ee56f1b166333c4e7a5
ynjacobs <ytj64611@gamil.com>
2019-07-08 18:09:58 -0400

    auto login v0.8

--- README.md
+++ README.md
@@ -66,3 +66,24 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+
+  1. Delete package-lock.json (not package.json!) and/or yarn.lock in your project folder.
+  2. Delete node_modules in your project folder.
+  3. Remove "jest" from dependencies and/or devDependencies in the package.json file in your project folder.
+  4. Run npm install or yarn, depending on the package manager you use.
+
+In most cases, this should be enough to fix the problem.
+If this has not helped, there are a few other things you can try:
+
+  5. If you used npm, install yarn (http://yarnpkg.com/) and repeat the above steps with it instead.
+     This may help because npm has known issues with package hoisting which may get resolved in future versions.
+
+  6. Check if /home/yjacobs/Responder/rapid-responder-fe/node_modules/jest is outside your project directory.
+     For example, you might have accidentally installed something in your home folder.
+
+  7. Try running npm ls jest in your project folder.
+     This will tell you which other package (apart from the expected react-scripts) installed jest.
+
+If nothing else helps, add SKIP_PREFLIGHT_CHECK=true to an .env file in your project.
+That would permanently disable this preflight check in case you want to proceed anyway.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import dotenv from 'dotenv';
+dotenv.config();
 
 function App() {
+// check the cookie
+// if it's created, send to the DJ server for auto-login ->
+// if it's not created, show the sign-up/sign-in page
+
+const url = process.env.REACT_APP_django_server
+
+axios.get(url, { 
+  headers: { 
+    'Access-Control-Allow-Origin': 'http://localhost:3000/', 
+    'Access-Control-Request-Headers':'origin'
+   } })
+  .then(response => {
+
+    const data = response.data;
+    const isAuth = data.Authenticated;
+    if(isAuth) {
+      const userType = data.flag;
+
+      if(userType === 'P') {
+      console.log('render Patient view');
+      }
+      else {
+        console.log('render Responder view');
+      }
+    }
+    else {
+      console.log('render the login/signup page');
+    }
+
+
+
+  })
+  .catch(error => {
+    console.log(error);
+  })
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+
+
+
+    </main>
   );
 }
 
