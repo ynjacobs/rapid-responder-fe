5b283cc86165561688c1c061e6fccb0f52902e8b
hassanashmawy <hassan.ashmawy@gmail.com>
2019-07-17 16:09:23 -0400

    90%

--- src/PatLand.js
+++ src/PatLand.js
@@ -1,59 +1,78 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-const PatLand = () => {
+class PatLand extends React.Component {
 
-    let patID = sessionStorage.getItem("userid");
-    console.log("patID:", patID);
+    constructor(props) {
+        super(props);
+        console.log('in PatLand constructor');
 
-    let [content, setContent] = useState();
+        this.patient = "null";
+        this.state = {
+            content: null,
+        };
+ 
+        let patID = sessionStorage.getItem("userid");
+        console.log("patID:", patID);
 
-    useEffect(() => {
         axios({
-            method: "GET",
-            url: `http://localhost:8000/cases/${patID}/get_unassign_cases_pat/`
-        }).then(res => {
-            const data = res.data;
-            const caze = data.case;
-            console.log("get_unassign_cases_pat==> data", data.case);
+            method:'GET',
+            url: `http://localhost:8000/patients/${patID}/`,
+        })
+        .then(res => {
+            this.patient = res.data;
+            // const resId = this.patient.id;
 
-            if(caze) {
-                // setInterval to check for assignment
-                console.log("I have a case");
-
-                setContent(() => {
-                    return (
-                        <div>
-                            <h1>You have called for help, enjoy the little time you have!</h1>
-                            <h2>{caze.description}</h2>
-                            <h2>{caze.patient.name} with {caze.condition["name"]}</h2>
-                        </div>
-                    )
-                });
-
-            } else {
-                console.log("I DON'T have cases :D ")
-                setContent(() => {
-                    return (
-                        <div>
-                            <h1>Welcome Patient</h1>
-                            <button className='button' onClick={handleHelp}>Emergency</button>
-                        </div>
-                        )
-                });
-            }
-        }).catch(err => {})
-    },{});
-
+            axios({
+                method: "GET",
+                url: `http://localhost:8000/cases/${patID}/get_unassign_cases_pat/`
+            }).then(res => {
+                const data = res.data;
+                const caze = data.case;
+                console.log("get_unassign_cases_pat==> data", data.case);
+                
+                if(caze) {
+                    // setInterval to check for assignment
+                    console.log("I have a case");
+                    let newVal;
+                    if(caze.status === 'UN'){
+                        newVal = (
+                            <div>
+                                <h1>You have called for help, enjoy the little time you have!</h1>
+                                <h2>{caze.description}</h2>
+                                <h2>{caze.patient.name} with {caze.condition["name"]}</h2>
+                            </div>
+                        );
+                    } else {
+                        newVal = (
+                            <div>
+                                <h1>You have called for help, and someone seems to care!</h1>
+                                <h2>{caze.responder.name} is coming for your help with {caze.condition["name"]} </h2>
+                            </div>
+                        );
+                    }
+                    this.setState({content: newVal})
     
-    const handleHelp = (event) => {
+                } else {
+                    console.log("I DON'T have cases :D ")
+                    let newVal = (
+                        <div>
+                            <button className='button' onClick={this.handleHelp}>Emergency</button>
+                        </div>
+                        );
+                    this.setState({content: newVal})
+
+                }}).catch(err => {console.log("error:", err)})
+            }).catch(err => {console.log("error:", err)})
+    }
+
+
+    handleHelp = (event) => {
         event.preventDefault();
         console.log("I need help!");
 
         let userid = sessionStorage.getItem("userid");
         console.log("userid:", userid);
-
-// axios().then().catch(err => {})
 
         axios({
             method:'GET',
@@ -62,10 +81,6 @@ const PatLand = () => {
             .then(res => {
                 const patient = res.data;
                 const condition = patient.conditions[0];
-
-                console.log("patient and cond", patient.conditions[0]);
-                console.log("patient cond", patient.conditions[0]);
-
 
                 axios({
                     method:'POST',
@@ -77,17 +92,19 @@ const PatLand = () => {
                     }
                     })
                     .then(res => {
-                        const patient = res.data;
-                        const condition = patient.conditions[0];
-        
-                        console.log("patient cond", patient.conditions[0]);
+                        const caze = res.data.case;
+
+                        let newVal = (
+                            <div>
+                                <h1>You have called for help, enjoy the little time you have!</h1>
+                                <h2>Condition: {caze.condition.name}</h2>
+                            </div>
+                        );
+                        this.setState({content: newVal});
                     })
-                    .catch(err => {})
-        
-
-
+                    .catch(err => {console.log("error:", err)})
             })
-            .catch(err => {})
+            .catch(err => {console.log("error:", err)})
             
         // console.log("patient cond:", patient.name);
 
@@ -95,11 +112,17 @@ const PatLand = () => {
 
     }
 
-    return (
-        <div>{content}</div>
-        
 
-    )
+    render(){
+        return (
+            <>
+            <h1>Welcome {this.patient.name}</h1>
+            <div>{this.state.content}</div>
+            </>
+    
+        )
+    }
 }
+
 
 export default PatLand;
--- src/ResLand.js
+++ src/ResLand.js
@@ -2,77 +2,114 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 class ResLand extends React.Component {
+
+    
     constructor(props) {
         super(props);
         console.log('in ResLand constructor');
-      
+
+        this.responder = "bla bla";
         this.state = {
             cases: null,
         };
     
         let userid = sessionStorage.getItem("userid");
         console.log("userid:", userid);
-    
-   
         axios({
             method:'GET',
             url: `http://localhost:8000/responders/${userid}/`,
             })
             .then(res => {
-               const responder = res.data;
-                const resId = responder.id;
+                this.responder = res.data;
+                const resId = this.responder.id;
                 axios({
                     method: "GET",
-                    url: `http://localhost:8000/cases/${resId}/get_unassign_cases/`,
+                    url: `http://localhost:8000/cases/${resId}/ongoing/`,
                 }).then(res => {
                     console.log("res.data", res.data);
-                    const cases = res.data;
-                    if(cases && cases.length > 0){
-                        // show the details of the case
-                    } else{
+                    const caze = res.data.case;
+                    if(caze){
+                        let casesNewVal;
+                        casesNewVal = (<h1> Run now to help {caze.patient.name} at address: {caze.patient.address}, 
+                         <br/>
+                         this person suffers from {caze.condition.name} 
+                         and requested help on {new Date(caze.creation_date).toLocaleTimeString()}</h1>
+                        );
+                        this.setState({cases: casesNewVal});
+                    } else {
                         // show all cases unassigned
                         axios({
                             method: "GET",
-                            url: `http://localhost:8000/cases/`,
+                            url: `http://localhost:8000/cases/unassigned/`,
                         }).then(res => {
-                            console.log("res.data", res.data);
-                            const data = res.data;
+                            console.log("unassigned cases=> res.data", res.data);
+                            const cases = res.data.cases;
                             let newVals = [];
-                            newVals = data.map((caze, index) => {
-                                return (
-                                    <div key={index}>
-                                        <h1>Help for {caze.patient.name}</h1> 
-                                        <button key={caze.id} onClick={(event) => this.acceptClicked(event, caze.id)}>accept</button>
-                                    </div>
-                                )
-                            });
+                            if(cases && cases.length > 0){
+                                newVals = cases.map((caze, index) => {
+                                    return (
+                                        <div key={index}>
+                                            <h1>Help for {caze.patient.name}</h1> 
+                                            <button key={caze.id} onClick={(event) => this.handleAcceptMission(event, caze.id)}>accept</button>
+                                        </div>
+                                    )
+                                });
+                            } else {
+                                newVals = <h1>No Urgent cases, you may relax!</h1>;
+                            }
+                           
                             this.setState({cases: newVals});
-                            console.log("cases", this.state.cases)
+                            // console.log("cases", this.state.cases)
                             
-                        })
-                        .catch(err => {})
+                        }).catch(err => {console.log("err::", err)})
                     }
-    
-                }).catch(err => {})
-            })
-            .catch(err => {})
+                }).catch(err => {console.log("err::", err)})
+            }).catch(err => {console.log("err::", err)})
 
     
       }
 
     acceptClicked = function(event, caseID){
-        event.preventDefault();
         console.log("mission accepted", caseID);
         this.props.handler(event, caseID);
     }
 
+    handleAcceptMission(event, caseID) {
+        event.preventDefault();
+        console.log("handleAcceptMission in ResLand", caseID);
+        const userID = sessionStorage.getItem("userid");
+      
+        axios({
+          method: 'PUT',
+          url: `http://localhost:8000/cases/${caseID}/`,
+          data: {
+            "res_id": userID,
+          }
+        })
+        .then(res => {
+            console.log("from handleAcceptMission res.data: ", res.data);
+            const caze = res.data.case;
+            let casesNewVal;
+        casesNewVal = (
+        <h1> 
+            Run now to help {caze.patient.name} at address: {caze.patient.address}, 
+            this person suffers from {caze.condition.name} and requested help on {caze.creation_date}
+        </h1>);
+            this.setState({cases: casesNewVal});
+            })
+        .catch(err => {
+          console.log("from handleAcceptMission error", err);
+        })
+      
+      
+      }
 
     render()
     {
         return (
             <div>
                 {/* <img className='' src={background_pic} alt=''/> */}
-                        <h1>Welcome Responder</h1>
+                        <h1>Welcome {this.responder.name}  </h1>
                         {this.state.cases }
             </div>
         )
