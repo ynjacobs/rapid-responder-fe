eee99b13f82e0649abbc490f4f1f8d4051855f9e
hassanashmawy <hassan.ashmawy@gmail.com>
2019-07-17 05:36:51 -0400

    50% core logic

--- src/PatLand.js
+++ src/PatLand.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const PatLand = () => {
@@ -6,14 +6,45 @@ const PatLand = () => {
     let patID = sessionStorage.getItem("userid");
     console.log("patID:", patID);
 
-    axios({
-        method: "GET",
-        url: `http://localhost:8000/cases/${patID}/get_unassign_cases_pat/`
-    }).then(res => {
-        const data = res.data;
-        console.log("data", data);
-    
-    }).catch(err => {})
+    let [content, setContent] = useState();
+
+    useEffect(() => {
+        axios({
+            method: "GET",
+            url: `http://localhost:8000/cases/${patID}/get_unassign_cases_pat/`
+        }).then(res => {
+            const data = res.data;
+            const caze = data.case;
+            console.log("get_unassign_cases_pat==> data", data.case);
+
+            if(caze) {
+                // message for searching for help
+                // setInterval to check for assignment
+                console.log("I have a case");
+
+                setContent(() => {
+                    return (
+                        <div>
+                            <h1>You have called for help, it's on the way or else!</h1>
+                            <h2>{caze.description}</h2>
+                            <h2>{caze.patient.name} with {caze.condition["name"]}</h2>
+                        </div>
+                        )
+                });
+
+            } else {
+                console.log("I DON'T have cases :D ")
+                setContent(() => {
+                    return (
+                        <div>
+                            <h1>Welcome Patient</h1>
+                            <button className='button' onClick={handleHelp}>Emergency</button>
+                        </div>
+                        )
+                });
+            }
+        }).catch(err => {})
+    },{});
 
     
     const handleHelp = (event) => {
@@ -66,13 +97,9 @@ const PatLand = () => {
     }
 
     return (
-        <div>
-            {/* <img className='' src={background_pic} alt=''/> */}
-                        
-        <h1>Welcome Patient</h1>
-        <button className='button' onClick={handleHelp}>Emergency</button>
+        <div>{content}</div>
+        
 
-        </div>
     )
 }
 
